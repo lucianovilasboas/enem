6d81d51bb4f8ddaa846dd7f0db65d2531df6c290
lucianovilasboas <lucianovilasboas@gmail.com>
2025-07-15 08:38:18 -0300

    updates

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ st.set_page_config(page_title="Evolução da Média no ENEM por Campus do IFMG",
 
 st.title("Evolução da Média no ENEM por Campus do IFMG (2014-2024)")
 
+st.markdown("""
+Este aplicativo permite visualizar a evolução da média das escolas do ENEM para os campi do IFMG e comparar 
+com as redes Estadual, Municipal e Privada.
+Selecione o campus e a área de conhecimento para ver os dados.
+""")
+
 # --------- Leitura do CSV ---------
 # Substitua pelo caminho correto do seu arquivo
 csv_file = 'enens2014-2024.csv'
@@ -39,10 +45,21 @@ cidades_ifmg = [
 ]
 
 cidades = sorted(cidades_ifmg)
+mapa_componentes = dict([('Média','MEDIA'), 
+                ('Linguagens, Códigos e suas Tecnologias','LC'), 
+                ('Ciências Humanas e suas Tecnologias','CH'), 
+                ('Ciências da Natureza e suas Tecnologias','CN'),
+                ('Matemática e suas Tecnologias', 'MT'), 
+                ('Redação','RD')])
 
-col = st.columns([2,4])
+col = st.columns([4,2])
 with col[0]:
-    cidade_sel = st.selectbox("Selecione a cidade:", cidades)
+    cidade_sel = st.selectbox("Selecione o Campus:", cidades)
+
+with col[1]:
+    componente_sel = st.selectbox("Selecione a Área de Conhecimento:", mapa_componentes.keys())
+
+
 
 df_filt = df[
     (df['NO_MUNICIPIO_ESC']== cidade_sel) & (df['SG_UF_ESC'] == 'MG') 
@@ -67,7 +84,7 @@ color_discrete_map = {
 fig = px.line(
     df_filt,
     x="ANO",
-    y="MEDIA",
+    y=mapa_componentes[componente_sel],
     color="LEGENDA",
     markers=True,
     title=f"{cidade_sel}: Evolução da Média das Escolas no ENEM (2014-2024)",
@@ -88,8 +105,8 @@ fig.update_layout(
 
 fig.update_traces(
     selector=dict(name=f'Campus {cidade_sel}'),
-    line=dict(width=4, color=color_discrete_map[f'Campus {cidade_sel}']),
-    marker=dict(size=8)
+    line=dict(width=5, color=color_discrete_map[f'Campus {cidade_sel}']),
+    marker=dict(size=8, symbol='x', color='black'),
 )
 
 st.plotly_chart(fig, use_container_width=True)
@@ -101,5 +118,8 @@ with st.expander("Ver tabela de dados filtrados", expanded=False):
 
 
 
-# st.caption("Desenvolvido com ❤️ por Luciano Espiridiao. 2025 - Todos os direitos reservados.")
+st.markdown("---")
+st.markdown("#### Desenvolvido a partir dos microdados do ENEM 2014-2024 disponibilizados pelo INEP em [gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/enem](https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/enem).")
+st.markdown("---")
+st.caption("Desenvolvido por [Luciano Espiridiao](luciano.espiriao@ifmg.edu.br). 2025 - Todos os direitos reservados.")
 
